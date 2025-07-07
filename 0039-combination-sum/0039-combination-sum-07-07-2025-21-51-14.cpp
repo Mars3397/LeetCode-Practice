18e0605a00b46ae4e683eaf72e8aec4fbe6c3c97
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-07 21:51:16 +0800

    Time: 0 ms (100%), Space: 14.4 MB (43.22%)

---
+++ 0039-combination-sum/0039-combination-sum-07-07-2025-21-51-14.cpp
@@ -0,0 +1,38 @@
+/*
+Approach
+1. use a backtracking approach to solve this problem
+
+Analysis
+1. time: O(n^2) where n = candidates.size()
+2. space: O(target / minimum in candidates)
+*/
+
+class Solution {
+private:
+    vector<vector<int>> ans;
+    int N, T;
+
+    void backtrack(vector<int> &candidates, vector<int> &cur, int start, int curSum) {
+        if (curSum == T) {
+            ans.emplace_back(cur);
+            return;
+        } else if (curSum > T) {
+            return;
+        }
+
+        for (int i = start; i < N; ++i) {
+            cur.push_back(candidates[i]);
+            backtrack(candidates, cur, i, curSum + candidates[i]);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        N = candidates.size(), T = target;
+        vector<int> cur;
+        backtrack(candidates, cur, 0, 0);
+
+        return ans;
+    }
+};
