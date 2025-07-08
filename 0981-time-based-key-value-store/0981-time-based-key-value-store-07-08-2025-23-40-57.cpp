a6a5d8394cede60ebe227a50d1046bc2c2df30ce
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-08 23:40:59 +0800

    Time: 62 ms (35.49%), Space: 139 MB (13.39%)

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store-07-08-2025-23-40-57.cpp
@@ -0,0 +1,43 @@
+/*
+Approach
+1. use a nested map to implement TimeMap where the key of first map is key and the second one is timestamp
+2. use lower_bound to find the largest timestamp smaller than given one when not exist
+*/
+
+class TimeMap {
+private:
+    unordered_map<string, map<int, string>> timeMap;
+
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        if (timeMap.count(key)) {
+            timeMap[key][timestamp] = value;
+        } else {
+            timeMap[key] = {{timestamp, value}};
+        }
+    }
+    
+    string get(string key, int timestamp) {
+        if (timeMap.count(key)) {
+            if (timeMap[key].count(timestamp)) {
+                return timeMap[key][timestamp];
+            } else {
+                auto it = timeMap[key].lower_bound(timestamp);
+                if (it != timeMap[key].begin()) {
+                    return (--it)->second;
+                }
+            }
+        }
+
+        return "";
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
