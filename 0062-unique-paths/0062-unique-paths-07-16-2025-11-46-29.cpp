ebd092082839b8684d3618f575fcd4387d7824b6
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-16 11:46:30 +0800

    Time: 0 ms (100%), Space: 9.4 MB (33.69%)

---
+++ 0062-unique-paths/0062-unique-paths-07-16-2025-11-46-29.cpp
@@ -0,0 +1,33 @@
+/*
+Approach
+1. althrough this problem can be directly calculate usign permutation
+2. we can use a 2D DP approach to avoid integer overflow
+3. dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+Analysis
+1. time: O(m*n)
+2. space: O(m*n)
+*/
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // init col 0 to all 1
+        for (int i = 0; i < m; ++i)
+            dp[i][0] = 1;
+
+        // init row 0 to all 1
+        for (int i = 0; i < n; ++i) 
+            dp[0][i] = 1;
+
+        // init the rest
+        for (int i = 1; i < m; ++i) 
+            for (int j = 1; j < n; ++j) 
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+
+        return dp[m-1][n-1];
+    }
+};
