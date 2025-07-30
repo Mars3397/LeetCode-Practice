c22fb7dbe488958c9804e0ba23ef8efc4e1de433
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-30 17:47:35 +0800

    Time: 7 ms (87.3%), Space: 13.9 MB (52.8%)

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring-07-30-2025-17-47-34.cpp
@@ -0,0 +1,47 @@
+/*
+Approach
+1. start from each s[i] as the middle of a palindromic to find the longest one
+2. handle odd length and even length differently
+
+Analysis
+1. time: O(n*2) where n = s.size()
+2. space: O(1)
+*/
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int left = 0, right = 0, n = s.size();
+        int maxLen = 0; string longest;
+        bool oddLen = true;
+
+        while (left < n || right < n) {
+            // find the longest palindromic on left and right
+            int halfLen = 0, l = left, r = right;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ++halfLen; --l; ++r;
+            }
+            
+            // calculate the full length
+            int fullLen;
+            if (oddLen) {
+                fullLen = halfLen * 2 - 1;
+                ++right;
+            } else {
+                fullLen = halfLen * 2;
+                ++left;
+            }
+
+            // save the logest one
+            if (fullLen > maxLen) {
+                maxLen = fullLen;
+                longest = s.substr(l+1, fullLen);
+            }
+
+            // flip the flag for increment of left or right
+            oddLen = !oddLen;
+        }
+
+        return longest;
+    }
+};
