6537e21e13103cb531c101e878c85ce52568e14e
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-31 00:35:09 +0800

    Time: 7 ms (82.64%), Space: 9.9 MB (97.99%)

---
+++ 0518-coin-change-ii/0518-coin-change-ii-07-31-2025-00-35-08.cpp
@@ -0,0 +1,30 @@
+/*
+Approach
+1. declare a vector with length = amount + 1 as the DP vector
+2. init the coins in DP vector to 1
+3. for each non-zero dp[i] -> add up all possible from dp[i-coins]
+
+Analysis
+1. time: (n * amount) where n = len(coins)
+2. space: O(amount)
+
+*/
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                if (dp[i - coin] < INT_MAX - dp[i]) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+        }
+
+
+        return dp[amount];
+    }
+};
