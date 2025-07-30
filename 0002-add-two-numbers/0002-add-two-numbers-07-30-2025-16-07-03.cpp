4879233dd5eb31fbbb7e400319778f5dd1ead40d
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-07-30 16:07:04 +0800

    Time: 0 ms (100%), Space: 76.4 MB (99.14%)

---
+++ 0002-add-two-numbers/0002-add-two-numbers-07-30-2025-16-07-03.cpp
@@ -0,0 +1,60 @@
+/*
+Apporoach
+1. record the carry in a boolean value
+2. add values from list2 to list1 and return the list1 head
+3. handle l1 + l2, l1, l2 or carry only
+
+Analysis
+1. time: O(max(n, m)) where n = len(l1), m = len(l2)
+2. space: O(1)
+*/
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1, *last = nullptr;
+        bool carry = false;
+
+        // add up same length digits
+        while (l1 || l2 || carry) {
+            if (l1 && l2) { // both have digits
+                l1->val += l2->val + carry;
+                if (l1->val >= 10) {
+                    l1->val -= 10;
+                    carry = true;
+                } else {
+                    carry = false;
+                }
+                last = l1;
+                l1 = l1->next;
+                l2 = l2->next;
+            } else if (l1) { // only l1
+                l1->val += carry;
+                if (l1->val >= 10) {
+                    l1->val -= 10;
+                    carry = true;
+                } else {
+                    carry = false;
+                }
+                last = l1;
+                l1 = l1->next;
+            } else if (l2) { // only l2
+                last->next = l2;
+                l2->val += carry;
+                if (l2->val >= 10) {
+                    l2->val -= 10;
+                    carry = true;
+                } else {
+                    carry = false;
+                }
+                last = last->next;
+                l2 = l2->next;
+            } else if (carry) { // only carry
+                last->next = new ListNode(1);
+                carry = false;
+            }
+        }
+
+        return head;
+    }
+};
