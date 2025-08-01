17c69841871d44ee42577c32744601b3c5f84c51
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-08-01 15:06:14 +0800

    Time: 0 ms (100%), Space: 8.6 MB (20.53%)

---
+++ 0007-reverse-integer/0007-reverse-integer-08-01-2025-15-06-13.cpp
@@ -0,0 +1,37 @@
+/*
+Approach
+1. determine the sign of x and then start reversing
+2. check the value when reversing, return 0 if exceed the boundary
+
+Analysis
+1. 
+*/
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = x % 10;
+        x /= 10;
+        
+        while (x < 0) {
+            int digit = x % 10;
+            if (ans < (INT_MIN - digit) / 10) {
+                return 0;
+            }
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+        
+        while (x > 0) {
+            int digit = x % 10;
+            if (ans > (INT_MAX - digit) / 10) {
+                return 0;
+            }
+            ans = ans * 10 + digit;
+            x /= 10;
+        }
+
+        return ans;
+    }
+};
