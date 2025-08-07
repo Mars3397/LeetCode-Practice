ca1c4c786f4285b7bf276bfee8c78bbd1c53da19
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-08-07 14:51:34 +0800

    Time: 74 ms (8.22%), Space: 36.1 MB (6.86%)

---
+++ 0049-group-anagrams/0049-group-anagrams-08-07-2025-14-51-33.cpp
@@ -0,0 +1,40 @@
+/*
+Approach
+1. compute the frequency of characters for each string and form a unique key for the anagram
+2. concate the frequency to a string to form a key
+3. store each anagram group in an unordered map
+
+Analysis
+1. time: O(n * m) where n = strs.size() and m = average of the length of string in strs
+2. space: O(n * m) where n = strs.size() and m = average of the length of string in strs
+*/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (string &str : strs) {
+            // count frequency
+            vector<int> freq(26, 0);
+            for (char c : str) {
+                ++freq[c - 'a'];
+            }
+
+            // form key
+            string key = "";
+            for (int i = 0; i < 26; ++i) {
+                key += to_string(freq[i]) + "#";
+            }
+
+            groups[key].push_back(move(str));
+        }
+
+        vector<vector<string>> ans;
+        for (auto [key, val] : groups) {
+            ans.push_back(move(val));
+        }
+
+        return ans;
+    }
+};
