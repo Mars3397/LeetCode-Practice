df246aae18005cd3283a780ebfe40469320791ac
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-08-08 16:11:38 +0800

    Time: 119 ms (30.77%), Space: 18.5 MB (14.45%)

---
+++ 0567-permutation-in-string/0567-permutation-in-string-08-08-2025-16-11-36.cpp
@@ -0,0 +1,43 @@
+/*
+Approach
+1. calculate the frequency of s1
+2. try to match the freqency for each substring start with a letter in s2
+
+Analysis
+1. time: 
+2. space: 
+*/
+
+class Solution {
+private:
+    int N1, N2;
+
+    bool match(vector<int> f, const string &s2, int start) {
+        for (int i = 0; i < N1; ++i) {
+            int idx = s2[start+i]-'a';
+            --f[idx];
+
+            if (f[idx] < 0) return false;
+        }
+
+        return true;
+    }
+
+public:
+    bool checkInclusion(string s1, string s2) {
+        N1 = s1.size(), N2 = s2.size();
+        if (N1 > N2) return false;
+
+        vector<int> freq(26, 0);
+        for (char c : s1) {
+            ++freq[c-'a'];
+        }
+
+        int end = N2 - N1;
+        for (int i = 0; i <= N2 - N1; ++i) {
+            if (freq[s2[i]-'a'] && match(freq, s2, i)) return true;
+        }
+
+        return false;
+    }
+};
