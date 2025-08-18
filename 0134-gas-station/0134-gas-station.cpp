ca50cd8c75b5ea8dfeed9157cd4b1a056785a762
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-08-18 17:43:52 +0800

    Time: 0 ms (100%), Space: 112.4 MB (71.77%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,32 @@
+/*
+Approach
+1. we should choose to start at the station that can fill tank with most gas within the sequence of station
+
+Analysis
+1. time: O(n) where n = gas.size()
+2. space: O(1)
+*/
+
+
+class Solution { 
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = 0, tank = 0, n = gas.size();
+
+        for (int cur = 0, start = 0; cur < n; ) {
+            start = cur;
+            tank = 0;
+
+            // travel to farthest station
+            while (tank >= 0 && cur - start < n) {
+                tank += gas[cur % n];
+                tank -= cost[cur % n];
+                ++cur;
+            }
+
+            if (tank >= 0) return start;
+        }
+
+        return -1;
+    }
+};
