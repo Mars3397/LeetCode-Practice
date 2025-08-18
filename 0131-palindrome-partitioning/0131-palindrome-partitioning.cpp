e34f5b60d5040f72d901723dffdf0adf93e9d1f2
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-08-18 16:32:37 +0800

    Time: 87 ms (18.94%), Space: 97.5 MB (17.77%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -4,8 +4,18 @@ Approach
 2. start from each index to expand palindrome -> if can, push to answer vector
 
 Analysis
-1. time: O(n^2) where n = s.size()
-2. space: O(1) (don't count return vector)
+1. time: O(n!) where n = s.size()
+2. space: O(n) (don't count return vector)
+*/
+
+/*
+Approach
+1. declare answer vector and push all char as vector to answer
+2. start from each index to expand palindrome -> if can, push to answer vector
+
+Analysis
+1. time: O(n!) where n = s.size()
+2. space: O(n) (don't count return vector)
 */
 
 class Solution {
@@ -28,7 +38,6 @@ private:
     void expand(int le, int ls, int rs, vector<vector<string>> &ans, string &s, int idx) {
         int n = ans[idx].size();
 
-        // cout << "check: " << le << " " << ls << " " << rs << " " << n << endl;
         while (le < ls && rs < n && isPalindrome(ans[idx][ls], ans[idx][rs])) {
             // push the current palindrome parition to answer vector
             ans.push_back({}); int i = ans.size()-1;
@@ -41,7 +50,6 @@ private:
                     ans[i].push_back(ans[idx][j]);
                 }
             }
-            // cout << "based on: " << idx << " -> " << ls << ", " << rs << endl;
 
             // keep expand based on current palindrome partition
             int size = ans[i].size();
