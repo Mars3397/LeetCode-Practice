a2926493832d437708e0e6083cd9d1c9a0937e48
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-09-29 14:52:10 +0800

    Time: 0 ms (100%), Space: 15.1 MB (65.9%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,12 +1,3 @@
-/*
-Approach
-1. solve with binary search (handle some special cases)
-2. case 1: |--o--x-----|
-3. case 2: |-----x--o--|
-
-*/
-
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -14,20 +5,15 @@ public:
 
         while (l <= r) {
             int mid = l + (r - l) / 2;
+
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < nums[l]) { // case 1
-                if (nums[mid] < target && target <= nums[r]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
-            } else { // case 2
-                if (nums[l] <= target && target < nums[mid]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+            } else if (nums[mid] >= nums[l]) {
+                if (nums[l] <= target && target < nums[mid]) r = mid - 1;
+                else l = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
+                else r = mid - 1;
             }
         }
 
