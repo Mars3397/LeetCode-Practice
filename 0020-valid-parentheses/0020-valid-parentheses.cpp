6a16af035d2f925f25e4e93d2b6a3beff7a72a42
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-10-03 22:04:26 -0700

    Time: 1 ms (17.4%), Space: 8.8 MB (54.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,34 +1,19 @@
-/*
-Approach
-1. use a stack to store the left parentheses for validation
-2. pop the stack top when encountering right parentheses to validate
-3. return false when invalid
-4. stack must be empty when finish checking all the characters
-
-Analysis
-1. time: O(n) where n = s.size()
-2. space: O(n) where n = s.size()
-*/
-
-
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        int n = s.size();
 
-        for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
-                stk.push(c);
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                stk.push(s[i]);
+            } else if (s[i] == ')') {
+                if (stk.empty() || stk.top() != '(') return false;
+                stk.pop();
+            } else if (s[i] == ']') {
+                if (stk.empty() || stk.top() != '[') return false;
+                stk.pop();
             } else {
-                // avoid use top when stack is empty
-                if (stk.empty()) return false;
-
-                // validate
-                if (c == ')' && stk.top() != '(') return false;
-                if (c == ']' && stk.top() != '[') return false;
-                if (c == '}' && stk.top() != '{') return false;
-
+                if (stk.empty() || stk.top() != '{') return false;
                 stk.pop();
             }
         }
