eaa81d4596b6a808aedbb073acc4886fb15c6c8c
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-10-04 10:47:21 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,22 +1,15 @@
-/*
-Approach
-1. +1 from the end of array and handle carry to the start of array
-2. insert 1 to the begin of array if has carry
-
-Analysis
-1. time: O(n) where n = digits.size()
-2. space: O(1)
-*/
-
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        bool carry = true;
-        for (int i = digits.size() - 1; i >= 0; --i) {
-            digits[i] += carry;
-            carry = digits[i] / 10;
+        int n = static_cast<int>(digits.size());
+        ++digits[n-1];
+
+        bool carry = false;
+        for (int i = n-1; i >= 0; --i) {
+            carry = (digits[i] / 10) > 0;
             digits[i] %= 10;
-            if (!carry) break;
+
+            if (!carry) return digits;
         }
 
         if (carry) {
