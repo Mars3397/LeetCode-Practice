e3e49b0c4876715750cea9c9d962a0ddf5f492f5
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-10-05 00:21:57 -0700

    Time: 2 ms (5.06%), Space: 7.7 MB (87.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,16 @@
-/*
-Approach
-1. dp approach
-2. dp[i] = dp[i-1] + dp[i-2] and dp[0] = 1, dp[1] = 1
-
-Analysis
-1. time: O(n)
-2. space: O(n)
-*/
-
-
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1);
-        dp[0] = 1; dp[1] = 1;
+        if (n <= 2) return n;
 
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i-1] + dp[i-2];
+        int prev2 = 1, prev1 = 2, cur;
+
+        for (int i = 3; i <= n; ++i) {
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
 
-        return dp[n];
+        return prev1;
     }
 };
