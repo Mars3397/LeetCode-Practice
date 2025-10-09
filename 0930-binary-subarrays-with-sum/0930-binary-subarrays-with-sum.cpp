88cdef84e441323b5d1c3dc5b61cee7795fb571c
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-10-08 22:15:07 -0700

    Time: 14 ms (34.68%), Space: 42.4 MB (5.14%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,41 +1,18 @@
-/*
-Approach
-1. # subarrays' sum = target => # subarrays' sum <= target - # subarrays' sum <= target - 1
-2. by converting the question into the approach in the above, we can use a sliding window approach to solve
-
-Analysis
-1. time: 
-2. space: 
-
-*/
-
-
 class Solution {
-private: 
-    int N;
-
-    int atMost(vector<int> &nums, int most) {
-        if (most < 0) return 0;
-        
-        int cnt = 0, sum = 0;
-
-        for (int l = 0, r = 0; r < N; ++r) {
-            sum += nums[r];
-
-            while (sum > most) {
-                sum -= nums[l++];
-            }
-
-            cnt += r - l + 1;
-        }
-
-        return cnt;
-    }
-
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        N = nums.size();
+        long long sum = 0;                 // 前綴和
+        int ans = 0;
+        unordered_map<long long,int> freq; // sum -> 次數
+        freq.reserve(nums.size() * 2);
+        freq[0] = 1;                       // 空前綴
 
-        return atMost(nums, goal) - atMost(nums, goal - 1);
+        for (int x : nums) {
+            sum += x;
+            if (auto it = freq.find(sum - goal); it != freq.end())
+                ans += it->second;         // 有幾個 sum-k
+            ++freq[sum];
+        }
+        return ans;
     }
 };
