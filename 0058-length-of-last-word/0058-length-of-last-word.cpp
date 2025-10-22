cd037a3959809b914e01560e6641571a7cab0a2b
郭昀 <96180941+Mars3397@users.noreply.github.com>
2025-10-21 21:29:35 -0700

    Time: 1 ms (5.07%), Space: 8.7 MB (90.5%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.size() - 1;
+
+        while (i >= 0 && s[i] == ' ') {
+            --i;
+        }
+
+        int end = i;
+
+        while (i >= 0 && s[i] != ' ') {
+            --i;
+        }
+
+        return end - i;
+    }
+};
